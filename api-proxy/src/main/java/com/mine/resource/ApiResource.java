a3a10fd1008f89cfc036e6c02edcbd252dc2e6dc
六翁 <lu.hl@alibaba-inc.com>
2016-05-31 15:26:29 +0800

    up for zk cloud

--- api-proxy/src/main/java/com/mine/resource/ApiResource.java
+++ api-proxy/src/main/java/com/mine/resource/ApiResource.java
@@ -3,12 +3,16 @@ package com.mine.resource;
 import com.mine.domain.AppUser;
 import org.jasig.cas.client.authentication.AttributePrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.security.cas.authentication.CasAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation;
@@ -17,10 +21,10 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 
 @Component
 @Path("/")
@@ -34,7 +38,7 @@ public class ApiResource {
     @GET
     @Path("health")
     @Produces(MediaType.APPLICATION_JSON)
-    public String  getHealth() {
+    public String getHealth() {
         return "Health: OK";
     }
 
@@ -43,26 +47,36 @@ public class ApiResource {
     @Path("users")
     @Produces(MediaType.APPLICATION_JSON)
     public List<AppUser> appAUsers() {
+        List<AppUser> result = new ArrayList<>();
         /*授权处理*/
         CasAuthenticationToken casAuthenticationToken = (CasAuthenticationToken) httpRequest.getUserPrincipal();
-        Map<String, Object> map =  getAttributes();
+        Map<String, Object> map = getAttributes();
 
         /*服务实例处理*/
         List<ServiceInstance> appAInstances = getAppInstaces("app-a");
         List<ServiceInstance> appBInstances = getAppInstaces("app-b");
 
         /*请求处理*/
+        URI appAUri = appAInstances.get(0).getUri();
+        List<AppUser> appAUserList = getAppUsers(appAUri);
+        URI appBUri = appBInstances.get(0).getUri();
+        List<AppUser> appBUserList = getAppUsers(appBUri);
+
+        /*数据处理*/
+        result.addAll(appAUserList);
+        result.addAll(appBUserList);
+        return result;
+    }
+
+    private List<AppUser> getAppUsers(URI appAUri) {
         Client c = ClientBuilder.newClient();
-        WebTarget target = c.target(appAInstances.get(0).getUri()).path("users");
+        WebTarget target = c.target(appAUri).path("users");
         Invocation.Builder request = target.request(MediaType.APPLICATION_JSON_TYPE);
         Response response = request.get();
         //AppUser appUser = response.readEntity(AppUser.class);
-        GenericType<List<AppUser>> genericType = new GenericType<List<AppUser>>() {};
-        List<AppUser> users = request.get(genericType);
-
-        /*数据处理*/
-
-        return users;
+        GenericType<List<AppUser>> genericType = new GenericType<List<AppUser>>() {
+        };
+        return request.get(genericType);
     }
 
     private List<ServiceInstance> getAppInstaces(String appName) {
