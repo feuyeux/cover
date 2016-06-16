6be9912937c217c5f42030430352db09ef950380
六翁 <lu.hl@alibaba-inc.com>
2016-06-16 15:09:42 +0800

    update

--- api-proxy/src/main/java/com/mine/resource/ApiResource.java
+++ api-proxy/src/main/java/com/mine/resource/ApiResource.java
@@ -183,12 +183,4 @@ public class ApiResource {
             return null;
         }
     }
-
-    public static void main(String[] args) {
-        AtomicInteger atomicInteger = new AtomicInteger(Integer.MAX_VALUE);
-        System.out.println(atomicInteger.get());
-        System.out.println(atomicInteger.incrementAndGet());
-        System.out.println(atomicInteger.incrementAndGet());
-        System.out.println(Integer.MIN_VALUE);
-    }
 }
